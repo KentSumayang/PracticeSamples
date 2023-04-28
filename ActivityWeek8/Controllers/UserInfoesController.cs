b99511fdb2bf7cf79f7d201c90843d54522717a1
Kent James Sumayang <kentjames2002@gmail.com>
2023-04-28 11:00:07 +0800

    edited ActivityWeek8

--- ActivityWeek8/Controllers/UserInfoesController.cs
+++ ActivityWeek8/Controllers/UserInfoesController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using ActivityWeek8.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ActivityWeek8.Models;
 
 namespace ActivityWeek8.Controllers
 {
@@ -14,7 +9,7 @@ namespace ActivityWeek8.Controllers
     public class UserInfoesController : ControllerBase
     {
         private readonly UserContext _context;
-        
+
 
         public UserInfoesController(UserContext context)
         {
@@ -22,19 +17,21 @@ namespace ActivityWeek8.Controllers
         }
 
         [HttpGet("GetUserInfo")]
-        public async Task<ActionResult<IEnumerable<UserInfo>>> Get() 
+        public async Task<ActionResult<IEnumerable<UserInfo>>> Get()
         {
             if (_context.UserInfos == null)
             {
                 return NotFound("Data Not Available");
             }
-            var properties = await _context.UserInfos.Include(a => a.UserOtherInfo).ToListAsync();
+            var properties = await _context.UserInfos
+                .Include(a => a.UserOtherInfo)
+                .ToListAsync();
             return properties;
         }
 
         // PUT: api/UserInfoes/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("EditUserOtherInfo")]
+        [HttpPut("EditUserInfo")]
         public async Task<IActionResult> PutUserInfo(UserInfo userInfo)
         {
 
@@ -42,16 +39,17 @@ namespace ActivityWeek8.Controllers
             {
                 _context.Entry(userInfo).State = EntityState.Modified;
                 _context.Entry(userInfo).Property(x => x.Id).IsModified = false;
-                _context.Entry(userInfo.UserOtherInfo).Property(x => x.Id).IsModified = false;
                 _context.Entry(userInfo).Property(x => x.LastName).IsModified = false;
                 _context.Entry(userInfo).Property(x => x.FirstName).IsModified = false;
+                _context.Entry(userInfo.UserOtherInfo).Property(x => x.Id).IsModified = false;
                 await _context.SaveChangesAsync();
             }
+
             catch (DbUpdateConcurrencyException)
             {
                 if (!UserInfoExists(userInfo.Id))
                 {
-                    return NotFound();
+                    return NotFound("User Not Found.");
                 }
                 else
                 {
@@ -68,30 +66,37 @@ namespace ActivityWeek8.Controllers
         [HttpPost("CreateUser")]
         public async Task<ActionResult<UserInfo>> PostUserInfo(UserInfo userInfo)
         {
-          if (_context.UserInfos == null)
-          {
-              return Problem("Entity set 'UserContext.UserInfos'  is null.");
-          }
+            if (_context.UserInfos == null)
+            {
+                return Problem("Table is empty. No data available");
+            }
+
             await _context.UserInfos.AddAsync(userInfo);
             userInfo.Status = 1;
             await _context.SaveChangesAsync();
 
             return Ok(userInfo);
         }
+
         [HttpPost("GetInfoViaUsernameAndPassword")]
-        public async Task<ActionResult<UserInfo>> GetInformation (string username, string password)
+        public async Task<ActionResult<UserInfo>> GetInformation(string username, string password)
         {
-            var exist_username = await _context.UserInfos.Where(x => x.Username == username).FirstOrDefaultAsync();
-            var exist_password = await _context.UserInfos.Where(x => x.Password == password).FirstOrDefaultAsync();
-            var properties = await _context.UserInfos.Include(a => a.UserOtherInfo)
-                .Where(x => x.Username == username).ToListAsync();
+            var exist_user = await _context.UserInfos
+                .Where(x => x.Username == username)
+                .Where(x => x.Password == password)
+                .FirstOrDefaultAsync();
+
+            var properties = await _context.UserInfos
+                .Include(a => a.UserOtherInfo)
+                .Where(x => x.Username == username)
+                .ToListAsync();
 
 
-            if (exist_username != null)
+            if (exist_user != null)
             {
-                if (exist_password != null)
+                if (exist_user != null)
                 {
-                    if (exist_password.Status == 0)
+                    if (exist_user.Status == 0)
                     {
                         return BadRequest("Invalid. Account has been Deactivated.");
                     }
@@ -105,18 +110,20 @@ namespace ActivityWeek8.Controllers
         [HttpPost("DeactivateUser")]
         public async Task<ActionResult<UserInfo>> SetUserStatusDeactive(string username, string password)
         {
-            var exist_username = await _context.UserInfos.Where(x => x.Username == username).FirstOrDefaultAsync();
-            var exist_password = await _context.UserInfos.Where(x => x.Password == password).FirstOrDefaultAsync();
-            
-            if (exist_username != null)
+            var exist_user = await _context.UserInfos
+                .Where(x => x.Username == username)
+                .Where(x => x.Password == password)
+                .FirstOrDefaultAsync();
+
+            if (exist_user != null)
             {
-                if (exist_password != null)
+                if (exist_user != null)
                 {
-                    if (exist_username.Status == 0)
+                    if (exist_user.Status == 0)
                     {
                         return Ok("Account is already inactive.");
                     }
-                    exist_username.Status = 0;
+                    exist_user.Status = 0;
                     await _context.SaveChangesAsync();
 
                     return Ok("Account has been deactivated.");
@@ -125,21 +132,24 @@ namespace ActivityWeek8.Controllers
             }
             return BadRequest("Username or Password is Invalid");
         }
+
         [HttpPost("ReactivateUser")]
         public async Task<ActionResult<UserInfo>> SetUserStatusActive(string username, string password)
         {
-            var exist_username = await _context.UserInfos.Where(x => x.Username == username).FirstOrDefaultAsync();
-            var exist_password = await _context.UserInfos.Where(x => x.Password == password).FirstOrDefaultAsync();
-            
-            if (exist_username != null)
+            var exist_user = await _context.UserInfos
+                .Where(x => x.Username == username)
+                .Where(x => x.Password == password)
+                .FirstOrDefaultAsync();
+
+            if (exist_user != null)
             {
-                if (exist_password != null)
+                if (exist_user != null)
                 {
-                    if (exist_username.Status == 1)
+                    if (exist_user.Status == 1)
                     {
                         return Ok("Account is already active.");
                     }
-                    exist_username.Status = 1;
+                    exist_user.Status = 1;
                     await _context.SaveChangesAsync();
 
                     return Ok("Account has been reactivated.");
